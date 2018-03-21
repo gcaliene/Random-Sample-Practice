9ec1ebe771764a1f7757e5758b7ff433fdd5c85e
Windows <gersoncalienes@gmail.com>
2018-03-20 23:53:40 -0400

    banana array

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
 // Make a keyless car EVEN BETTER!
-// We are improving our car from previous exercise now.
-// Solutions to future exercises will be in the .js file (meaning Javascript file) from now on.
+// // We are improving our car from previous exercise now.
+// // Solutions to future exercises will be in the .js file (meaning Javascript file) from now on.
 
-const checkDriverAge = age => {
-  if (age < 18) {
-    alert('Sorry, you are too yound to drive this car. Powering off');
-  } else if (age > 18) {
-    alert('Powering On. Enjoy the ride!');
-  } else if (age === 18) {
-    alert('Congratulations on your first year of driving. Enjoy the ride!');
-  }
-};
+// const checkDriverAge = age => {
+//   if (age < 18) {
+//     alert('Sorry, you are too yound to drive this car. Powering off');
+//   } else if (age > 18) {
+//     alert('Powering On. Enjoy the ride!');
+//   } else if (age === 18) {
+//     alert('Congratulations on your first year of driving. Enjoy the ride!');
+//   }
+// };
 
-// 1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
-// Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
+// // 1. Make the above code have a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. Use Function Declaration to create this function.
+// // Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
 
-// 2. Create another function that does the same thing, called checkDriverAge2() using Function Expression.
+// // 2. Create another function that does the same thing, called checkDriverAge2() using Function Expression.
 
-// BONUS: Instead of using the prompt. Now, only use the return keyword and make the checkDriverAge() function accept an argument of age, so that if you enter:
-checkDriverAge(92);
-// it returns "Powering On. Enjoy the ride!"
+// // BONUS: Instead of using the prompt. Now, only use the return keyword and make the checkDriverAge() function accept an argument of age, so that if you enter:
+// checkDriverAge(92);
+// // it returns "Powering On. Enjoy the ride!"
+
+// using this array,
+// var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// 1. Remove the Banana from the array.
+
+// 2. Sort the array in order.
+
+// 3. Put "Kiwi" at the end of the array.
+
+// 4. Remove "Apples" from the array.
+
+// 5. Sort the array in reverse order.
+
+// you should have at the end:
+['Oranges', 'Kiwi', 'Blueberries'];
+
+// using this array,
+// var array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges".
